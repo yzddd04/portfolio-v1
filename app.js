6c3fa6562fd015735ea56a0531ec014ad58ea47e
yzddd04 <ahmadyazidarifuddin04@gmail.com>
2025-03-25 02:27:58 +0700

    update

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const video2 = document.getElementById('projectVideo2');
 const video3 = document.getElementById('projectVideo3');
 const hoverSign = document.querySelector('.hover-sign');
 
+
+//sidebar elements //
+const sidebar = document.querySelector('.sidebar');
+const menu = document.querySelector('.menu-icon');
+const close = document.querySelector('.close-icon');
+
+
 const videoList = [video1, video2, video3];
 
 videoList.forEach(function(video){
@@ -14,4 +21,15 @@ videoList.forEach(function(video){
         video.pause();
         hoverSign.classList.remove('active')
     })
+})
+
+// sidebar elements //
+menu.addEventListener("click", function(){
+    sidBar.classList.add("close-sidebar");
+    sideBar.classList.add("open-sidebar");
+})
+
+close.addEventListener("click", function(){
+    sidBar.classList.remove("open-sidebar");
+    sideBar.classList.remove("close-sidebar");
 })
